6d7488351923173b26aa8a72341a01b9d04ec216
Mike1ife <132564989+Mike1ife@users.noreply.github.com>
2025-12-29 14:17:12 -0500

    Delete Frontend directory

--- Frontend/components/modals/ModalHeader.tsx
+++
@@ -1,8 +0,0 @@
-export default function ModalHeader({ title, onClose }: { title: string, onClose: Function; }) {
-    return (
-        <header className="modal-header">
-            <h3>{title}</h3>
-            <button onClick={() => onClose()}>×</button>
-        </header>
-    );
-}
